16b621664f70f250fa7acfa2c800173886f97e35
amanthukral12 <thukral618@gmail.com>
2022-11-22 18:20:00 +0530

    Adding basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import "./App.css";
+import Folder from "./components/Folder";
+import explorer from "./Data/folderData";
 
 function App() {
-  return <div className="App"></div>;
+  const [explorerData, setExplorerData] = useState(explorer);
+
+  return (
+    <div className="App">
+      <Folder explorer={explorerData} />
+    </div>
+  );
 }
 
 export default App;
